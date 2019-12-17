factorized functions in utils.py, made PixelCNN softmax, fixed gradient flow in VQVAE, finished vqvae_loss

pixelcnn.py
@@ -1,15 +1,12 @@
-# import sys
-
 import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.utils import Progbar
-# from tensorflow.keras import regularizers
-# from tensorflow.keras.layers import Layer
-# from PIL import Image
+
 from datetime import datetime
 import numpy as np
 from tensorflow.python.ops import array_ops
+from utils import training_step, get_binarized_mnist
 
 
 class MonoMaskedConv2D(Conv2D):
@@ -105,42 +102,34 @@ class PixelCNN(Model):
         for i in range(self.x_size):
             for j in range(self.x_size):
                 logits = self.call(current_sample)
-                prob_cond = tf.nn.sigmoid(logits[0, i, j, 0])
-                current_sample[0, i, j, 0] = self.multinomial_sample([1. - prob_cond, prob_cond])
+                prob_cond = tf.nn.softmax(logits[0, i, j, :])
+                current_sample[0, i, j, 0] = self.multinouli_sample(prob_cond)
                 prog.update(i*self.x_size+j)
         return current_sample
 
     @staticmethod
-    def multinomial_sample(dist):
+    def multinouli_sample(dist):
         dist = np.array(dist)
         dist /= np.sum(dist)
         return np.random.choice(len(dist), p=dist)
 
 
 @tf.function
-def compute_loss(inputs, model):
+def pixelcnn_loss(inputs, model):
     logits = model(inputs)
-    nll = tf.nn.sigmoid_cross_entropy_with_logits(labels=inputs, logits=logits)
-    loss = tf.reduce_sum(nll, axis=[1, 2, 3])
+    inputs = tf.cast(inputs[:, :, :, 0], tf.int32)
+    nll = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=inputs, logits=logits)
+    loss = tf.reduce_sum(nll, axis=[1, 2])
     return tf.reduce_mean(loss)
-
-
-@tf.function
-def training_step(x, model, optimizer):
-    with tf.GradientTape() as tape:
-        loss = compute_loss(x, model)
-    grads = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-    return loss
 
 
 if __name__ == '__main__':
     IMAGE_SIZE = 28
     FEATURE_MAPS = 16
-    OUTPUT_MAPS = 1
+    OUTPUT_MAPS = 2
     DEPTH = 4
     EPOCHS = 10
-    BATCH_SIZE = 1000
+    BATCH_SIZE = 1024
     BUFFER_SIZE = 60000
 
     now = datetime.now()
@@ -148,53 +137,34 @@ if __name__ == '__main__':
     SAVE_FILE = './checkpoints/pixelcnn_' + timestamp
     TENSORBOARD_DIR = './tensorboard/' + timestamp
 
-    file_writer = tf.summary.create_file_writer(TENSORBOARD_DIR)
-
-    # dataset
-    (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
-    x_train = x_train[..., np.newaxis].astype('float32')
-    x_test = x_test[..., np.newaxis].astype('float32')
-
-    # we have to perform quantization of the pixels
-
-    x_train /= 255.
-    x_test /= 255.
-
-    x_train[x_train >= 0.5] = 1
-    x_train[x_train < 0.5] = 0
-    x_test[x_test >= 0.5] = 1
-    x_test[x_test < 0.5] = 0
-
-    train_dataset = tf.data.Dataset.from_tensor_slices(x_train).batch(BATCH_SIZE)
-    test_dataset = tf.data.Dataset.from_tensor_slices(x_test).batch(BATCH_SIZE)
+    # file_writer = tf.summary.create_file_writer(TENSORBOARD_DIR)
+    train_dataset, test_dataset, train_size, _ = get_binarized_mnist(BATCH_SIZE, BUFFER_SIZE)
 
     pixel_cnn = PixelCNN(IMAGE_SIZE, DEPTH, FEATURE_MAPS, OUTPUT_MAPS)
     adam = tf.keras.optimizers.Adam()
     loss_mean = tf.keras.metrics.Mean()
 
     for epoch in range(1, EPOCHS + 1):
-        prog = Progbar(x_train.shape[0] / BATCH_SIZE)
+        prog = Progbar(train_size / BATCH_SIZE)
 
         # train
         for step, input in enumerate(train_dataset):
-            loss = training_step(input, pixel_cnn, adam)
-
+            loss = training_step(input, pixel_cnn, adam, pixelcnn_loss)
             logits = pixel_cnn(input)
-            with file_writer.as_default():
-                tf.summary.image('input', input[0][tf.newaxis, ...], step=step+1)
-                tf.summary.histogram('input_hist', input[0, :, :, 0], step=step+1)
-                tf.summary.image('prob', tf.nn.sigmoid(logits)[0][tf.newaxis, ...], step=step+1)
-                tf.summary.histogram('prob_hist', tf.nn.sigmoid(logits)[0, :, :, 0], step=step+1)
-
+            # with file_writer.as_default():
+            #    tf.summary.image('input', input[0][tf.newaxis, ...], step=step+1)
+            #    tf.summary.histogram('input_hist', input[0, :, :, 0], step=step+1)
+            #    tf.summary.image('prob', tf.nn.sigmoid(logits)[0][tf.newaxis, ...], step=step+1)
+            #    tf.summary.histogram('prob_hist', tf.nn.sigmoid(logits)[0, :, :, 0], step=step+1)
             prog.update(step, [('loss', loss)])
 
         # test
         for input in test_dataset:
-            loss_mean(compute_loss(input, pixel_cnn))
+            loss_mean(pixelcnn_loss(input, pixel_cnn))
         print(f'\nEpoch {epoch} - NLL loss: {loss_mean.result().numpy()}')
         loss_mean.reset_states()
 
-        sample = pixel_cnn.sample()
-        with file_writer.as_default():
-            tf.summary.image('sample', sample, step=epoch)
-            tf.summary.histogram('sample_hist', sample, step=epoch)
+        # sample = pixel_cnn.sample()
+        # with file_writer.as_default():
+        #     tf.summary.image('sample', sample, step=epoch)
+        #     tf.summary.histogram('sample_hist', sample, step=epoch)

vqvae.py
@@ -1,9 +1,13 @@
+from datetime import datetime
+
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose
 from tensorflow.keras import Model
+from tensorflow_core.python.keras.utils import Progbar
 
-from pixelcnn import PixelCNN
+from pixelcnn import PixelCNN, pixelcnn_loss
+from utils import training_step, get_binarized_mnist
 
 
 class VectorQuantization(Layer):
@@ -17,21 +21,22 @@ class VectorQuantization(Layer):
         self.codebook = self.add_weight(name='codebook', shape=(self.k, self.d),
                                         initializer=initializer, trainable=True)
 
-    def call(self, inputs, **kwargs):
+    def call(self, z_e, **kwargs):
+        z_e_sg = tf.stop_gradient(z_e)
         cb = tf.reshape(self.codebook, (1, 1, 1, self.k, self.d))
-        inputs = inputs[..., tf.newaxis, :]
-        diff = inputs - cb
+        z_e_sg = z_e_sg[..., tf.newaxis, :]
+        diff = z_e_sg - cb
         norm = tf.norm(diff, axis=-1)
-        index = tf.argmin(norm, axis=-1)
-        return index
+        q_z = tf.argmin(norm, axis=-1)
+        return q_z
 
     def sample(self, q_z):
-        one_hot = tf.one_hot(q_z)
+        one_hot = tf.one_hot(q_z, self.k)
         one_hot = one_hot[..., tf.newaxis, :]
         cb = tf.transpose(self.codebook)
         cb = tf.reshape(cb, (1, 1, 1, self.d, self.k))
-        sample = tf.reduce_sum(cb * one_hot, axis=-1)
-        return sample
+        z_q = tf.reduce_sum(cb * one_hot, axis=-1)
+        return z_q
 
 
 class Encoder(Model):
@@ -55,40 +60,113 @@ class Decoder(Model):
         self.hidden_layers = []
         for n in hidden_layers:
             self.hidden_layers.append(Conv2DTranspose(n, 3, strides=(2, 2), padding='SAME', activation='relu'))
-        self.output_layer = Conv2DTranspose(x_size, 3, strides=(1, 1), padding='SAME', activation='sigmoid')
+        self.output_layer = Conv2DTranspose(x_size, 3, strides=(1, 1), padding='SAME')
 
     def call(self, x, **kwargs):
         for hidden_layer in self.hidden_layers:
             x = hidden_layer(x)
-        x = self.output_layer(x)
-        return x
+        logits = self.output_layer(x)
+        return logits
 
 
 class VQVAE(Model):
     def __init__(self, d, k):
         super(VQVAE, self).__init__()
+        self.d = d
+        self.k = k
         self.encoder = Encoder(d, hidden_layers=[16, 32])
         self.vq = VectorQuantization(k)
         self.decoder = Decoder(1, hidden_layers=[32, 16])
-        self.pixelcnn = PixelCNN(28, 4, 16, 2)
+        # PixelCNN dimension is latent dimension: 7
+        self.pixelcnn = PixelCNN(7, 4, 16, d)
 
     def call(self, inputs, **kwargs):
         z_e = self.encoder(inputs)
         q_z = self.vq(z_e)
         z_q = self.vq.sample(q_z)
-        z_q = z_e + tf.stop_gradient(z_q - z_e)
-        x = self.decoder(z_q)
-        return x
+        z_q_sg = z_e + tf.stop_gradient(z_q - z_e)
+        logits = self.decoder(z_q_sg)
+        return logits, z_e, z_q, q_z
 
     def sample(self):
-        self.
-
+        q_z = self.pixelcnn.sample()
+        z_q = self.vq.sample(q_z)
+        logits = self.decoder(z_q)
+        return tf.nn.softmax(logits)
 
 
 @tf.function
-def compute_loss(input, model):
-    
+def vqvae_loss(input, model, beta=0.25):
+    logits, z_e, z_q, _ = model(input)
+
+    print('z_e', z_e.shape)
+    print('z_q', z_q.shape)
+    # log p(x|z_q) reconstruction loss
+    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(input, logits)
+    logp_x_z = tf.reduce_sum(cross_entropy, axis=[1, 2, 3])
+
+    # vector quantization loss
+    vq_loss = tf.norm(tf.stop_gradient(z_e) - z_q, axis=-1) ** 2
+    vq_loss = tf.reduce_sum(vq_loss, axis=[1, 2])
+
+    # # commitment loss
+    commitment_loss = tf.norm(z_e - tf.stop_gradient(z_q), axis=-1) ** 2
+    commitment_loss = tf.reduce_sum(commitment_loss, axis=[1, 2])
+    return tf.reduce_mean(logp_x_z + vq_loss + beta*commitment_loss)
 
 
 if __name__ == '__main__':
-    ...
+
+    IMAGE_SIZE = 28
+    EPOCHS_VQVAE = 10
+    EPOCHS_PIXELCNN = 10
+    BATCH_SIZE = 1024
+    BUFFER_SIZE = 60000
+    D = 64
+    K = 100
+    BETA = 0.25
+
+    now = datetime.now()
+    timestamp = str(now.strftime("%Y%m%d_%H-%M-%S"))
+    SAVE_FILE = './checkpoints/vqvae_' + timestamp
+    TENSORBOARD_DIR = './tensorboard/' + timestamp
+
+    file_writer = tf.summary.create_file_writer(TENSORBOARD_DIR)
+    train_dataset, test_dataset, train_size, _ = get_binarized_mnist(BATCH_SIZE, BUFFER_SIZE)
+
+    vqvae = VQVAE(D, K)
+    adam = tf.keras.optimizers.Adam()
+    loss_mean = tf.keras.metrics.Mean()
+
+    for epoch in range(1, EPOCHS_VQVAE + 1):
+        prog = Progbar(train_size / BATCH_SIZE)
+
+        # train
+        for step, input in enumerate(train_dataset):
+            loss = training_step(input, vqvae, adam, vqvae_loss)
+            prog.update(step, [('loss', loss)])
+
+        # test
+        for input in test_dataset:
+            loss_mean(vqvae_loss(input, vqvae))
+        print(f'\nVQVAE - Epoch {epoch} - Test NLL loss: {loss_mean.result().numpy()}')
+        loss_mean.reset_states()
+
+    # then you have to train pixelcnn
+    for epoch in range(1, EPOCHS_PIXELCNN + 1):
+        prog = Progbar(train_size / BATCH_SIZE)
+
+        # train
+        for step, input in enumerate(train_dataset):
+            _, _, _, q_z = vqvae(input)
+            q_z = q_z[..., tf.newaxis]
+            loss = training_step(q_z, vqvae.pixelcnn, adam, pixelcnn_loss)
+            prog.update(step, [('loss', loss)])
+
+        # test
+        for input in test_dataset:
+            _, _, _, q_z = vqvae(input)
+            q_z = q_z[..., tf.newaxis]
+            loss_mean(pixelcnn_loss(q_z, vqvae.pixelcnn))
+        print(f'\nPixelCNN - Epoch {epoch} - Test NLL loss: {loss_mean.result().numpy()}')
+        loss_mean.reset_states()