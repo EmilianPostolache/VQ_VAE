f3c594676cbcca99c6f651a05a9d453f6385584f
Emilian Postolache <emilianpos@gmail.com>
2019-12-17 12:22:52 +0100

    fixed pixelcnn error on sampling, added vqvae (not finished)

--- pixelcnn.py
+++ pixelcnn.py
@@ -1,11 +1,15 @@
+# import sys
+
 import tensorflow as tf
-# tf.enable_eager_execution()
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.utils import Progbar
-from tensorflow.keras import regularizers
+# from tensorflow.keras import regularizers
+# from tensorflow.keras.layers import Layer
 # from PIL import Image
+from datetime import datetime
 import numpy as np
+from tensorflow.python.ops import array_ops
 
 
 class MonoMaskedConv2D(Conv2D):
@@ -27,37 +31,43 @@ class MonoMaskedConv2D(Conv2D):
             self.mask[center, center, :, :] = 0.
 
     def call(self, inputs, **kwargs):
-        x = tf.nn.conv2d(inputs, self.kernel * self.mask, strides=self.strides, padding=str.upper(self.padding))
+        outputs = self._convolution_op(inputs, self.kernel * self.mask)
+
         if self.use_bias:
-            x += self.bias
-        return x
+            if self.data_format == 'channels_first':
+                if self.rank == 1:
+                    # nn.bias_add does not accept a 1D input tensor.
+                    bias = array_ops.reshape(self.bias, (1, self.filters, 1))
+                    outputs += bias
+                else:
+                    outputs = tf.nn.bias_add(outputs, self.bias, data_format='NCHW')
+            else:
+                outputs = tf.nn.bias_add(outputs, self.bias, data_format='NHWC')
+
+        if self.activation is not None:
+            return self.activation(outputs)
+        return outputs
 
 
 class ResidualBlock(Model):
-    def __init__(self, h_size):
-        super(ResidualBlock, self).__init__()
+    def __init__(self, h_size, **kwargs):
+        super(ResidualBlock, self).__init__(**kwargs)
         self.h_size = h_size
-        self.relu1 = tf.keras.layers.ReLU()
-        self.conv1 = tf.keras.layers.Conv2D(2*self.h_size, 1, padding='SAME')
-        self.relu2 = tf.keras.layers.ReLU()
-        self.conv2 = MonoMaskedConv2D(self.h_size, 3, mask_type='B', padding='SAME')
-        self.relu3 = tf.keras.layers.ReLU()
-        self.conv3 = tf.keras.layers.Conv2D(2*self.h_size, 1, padding='SAME')
+        self.conv1 = tf.keras.layers.Conv2D(2*self.h_size, 1, padding='same', activation='relu')
+        self.conv2 = MonoMaskedConv2D(self.h_size, 3, mask_type='B', padding='same', activation='relu')
+        self.conv3 = tf.keras.layers.Conv2D(2*self.h_size, 1, padding='same', activation='relu')
 
     def call(self, inputs, **kwargs):
-        x = self.relu1(inputs)
-        x = self.conv1(x)
-        x = self.relu2(x)
+        x = self.conv1(inputs)
         x = self.conv2(x)
-        x = self.relu3(x)
         x = self.conv3(x)
         x += inputs
         return x
 
 
 class ResidualBlockSequence(Model):
-    def __init__(self, n, h_size):
-        super(ResidualBlockSequence, self).__init__()
+    def __init__(self, n, h_size, **kwargs):
+        super(ResidualBlockSequence, self).__init__(**kwargs)
         self.n = n
         self.h_size = h_size
         self.res_list = [ResidualBlock(h_size) for _ in range(n)]
@@ -70,47 +80,41 @@ class ResidualBlockSequence(Model):
 
 class PixelCNN(Model):
 
-    def __init__(self, x_size, n, h_size, d_size):
-        super(PixelCNN, self).__init__()
+    def __init__(self, x_size, n, h_size, d_size, **kwargs):
+        super(PixelCNN, self).__init__(**kwargs)
         self.x_size = x_size
         self.h_size = h_size
         self.d_size = d_size
         self.n = n
-        self.conv1 = MonoMaskedConv2D(2*h_size, x_size*2+1, mask_type='A', padding='SAME',
-                                      kernel_regularizer=regularizers.l2(0.01))
+        self.conv1 = MonoMaskedConv2D(2*h_size, 7, mask_type='A', padding='same', activation='relu')
         self.resnet = ResidualBlockSequence(n, h_size)
-        self.relu1 = tf.keras.layers.ReLU()
-        self.conv2 = Conv2D(2*h_size, 1, padding='SAME')  # , kernel_regularizer=regularizers.l2(0.01))
-        self.relu2 = tf.keras.layers.ReLU()
-        self.conv3 = Conv2D(d_size, 1, padding='SAME')  # , kernel_regularizer=regularizers.l2(0.01))
-        # self.softmax = tf.keras.layers.Softmax()
+        self.conv2 = Conv2D(2*h_size, 1, padding='same', activation='relu')
+        self.conv3 = Conv2D(d_size, 1, padding='same')  # , activation='sigmoid')
 
     def call(self, inputs, **kwargs):
         x = self.conv1(inputs)
         x = self.resnet(x)
-        x = self.relu1(x)
         x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.conv3(x)
-        # x = self.softmax(x)
-        return x
+        logits = self.conv3(x)
+        return logits
 
     def sample(self):
-        current_sample = np.zeros((self.x_size, self.x_size))[np.newaxis, ..., np.newaxis].astype('float32')
+        current_sample = np.zeros((self.x_size, self.x_size))[np.newaxis, ..., np.newaxis].astype(np.float32)
         print('\nSampling...')
         prog = Progbar(self.x_size ** 2)
         for i in range(self.x_size):
             for j in range(self.x_size):
                 logits = self.call(current_sample)
-                print(tf.nn.sigmoid(logits))
-                current_sample[0, i, j, 0] = 1 if tf.nn.sigmoid(logits)[0, i, j, 0] >= 0.5 else 0
-                # self.multinomial_sample(prob_cond[0, i, j, :])
+                prob_cond = tf.nn.sigmoid(logits[0, i, j, 0])
+                current_sample[0, i, j, 0] = self.multinomial_sample([1. - prob_cond, prob_cond])
                 prog.update(i*self.x_size+j)
         return current_sample
 
-    # def multinomial_sample(self, dist):
-    #    return np.array([np.random.choice(self.d_size, p=d) for d in dist]).astype('float32')
-    #    return np.random.choice(self.d_size, p=dist.numpy())
+    @staticmethod
+    def multinomial_sample(dist):
+        dist = np.array(dist)
+        dist /= np.sum(dist)
+        return np.random.choice(len(dist), p=dist)
 
 
 @tf.function
@@ -118,10 +122,7 @@ def compute_loss(inputs, model):
     logits = model(inputs)
     nll = tf.nn.sigmoid_cross_entropy_with_logits(labels=inputs, logits=logits)
     loss = tf.reduce_sum(nll, axis=[1, 2, 3])
-    return tf.reduce_mean(loss) + tf.add_n(model.losses)
-    # p_conditional = tf.gather_nd(p_conditional, tf.cast(inputs, tf.int32), batch_dims=3)
-    # log_likelihood = -tf.reduce_sum(tf.math.log(p_conditional), axis=[1, 2])
-    # return tf.reduce_mean(log_likelihood)
+    return tf.reduce_mean(loss)
 
 
 @tf.function
@@ -142,13 +143,12 @@ if __name__ == '__main__':
     BATCH_SIZE = 1000
     BUFFER_SIZE = 60000
 
-    pixel_cnn = PixelCNN(IMAGE_SIZE, DEPTH, FEATURE_MAPS, OUTPUT_MAPS)
-
-    from datetime import datetime
-
     now = datetime.now()
-    timestamp = str(now.strftime("%Y-%m-%d_%H-%M-%S"))
+    timestamp = str(now.strftime("%Y%m%d_%H-%M-%S"))
     SAVE_FILE = './checkpoints/pixelcnn_' + timestamp
+    TENSORBOARD_DIR = './tensorboard/' + timestamp
+
+    file_writer = tf.summary.create_file_writer(TENSORBOARD_DIR)
 
     # dataset
     (x_train, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
@@ -165,12 +165,11 @@ if __name__ == '__main__':
     x_test[x_test >= 0.5] = 1
     x_test[x_test < 0.5] = 0
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-    print(train_dataset.shape)
-    test_dataset = tf.data.Dataset.from_tensor_slices(x_test).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    train_dataset = tf.data.Dataset.from_tensor_slices(x_train).batch(BATCH_SIZE)
+    test_dataset = tf.data.Dataset.from_tensor_slices(x_test).batch(BATCH_SIZE)
 
+    pixel_cnn = PixelCNN(IMAGE_SIZE, DEPTH, FEATURE_MAPS, OUTPUT_MAPS)
     adam = tf.keras.optimizers.Adam()
-
     loss_mean = tf.keras.metrics.Mean()
 
     for epoch in range(1, EPOCHS + 1):
@@ -179,6 +178,14 @@ if __name__ == '__main__':
         # train
         for step, input in enumerate(train_dataset):
             loss = training_step(input, pixel_cnn, adam)
+
+            logits = pixel_cnn(input)
+            with file_writer.as_default():
+                tf.summary.image('input', input[0][tf.newaxis, ...], step=step+1)
+                tf.summary.histogram('input_hist', input[0, :, :, 0], step=step+1)
+                tf.summary.image('prob', tf.nn.sigmoid(logits)[0][tf.newaxis, ...], step=step+1)
+                tf.summary.histogram('prob_hist', tf.nn.sigmoid(logits)[0, :, :, 0], step=step+1)
+
             prog.update(step, [('loss', loss)])
 
         # test
@@ -187,9 +194,7 @@ if __name__ == '__main__':
         print(f'\nEpoch {epoch} - NLL loss: {loss_mean.result().numpy()}')
         loss_mean.reset_states()
 
-        sample = pixel_cnn.sample()[0, :, :, 0];
-        sample[sample == 1] = 255
-        print(sample)
-
-        pixel_cnn.save_weights(SAVE_FILE)
-        print('Model weights have been successfully saved in ' + SAVE_FILE)
+        sample = pixel_cnn.sample()
+        with file_writer.as_default():
+            tf.summary.image('sample', sample, step=epoch)
+            tf.summary.histogram('sample_hist', sample, step=epoch)
---
+++ vqvae.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose
+from tensorflow.keras import Model
+
+
+class VectorQuantization(Layer):
+    def __init__(self, k, **kwargs):
+        super(VectorQuantization, self).__init__(**kwargs)
+        self.k = k
+
+    def build(self, input_shape):
+        self.d = input_shape[-1]
+        initializer = tf.keras.initializers.GlorotUniform()
+        self.codebook = self.add_weight(name='codebook', shape=(self.k, self.d),
+                                        initializer=initializer, trainable=True)
+
+    def call(self, inputs, **kwargs):
+        cb = tf.reshape(self.codebook, (1, 1, 1, self.k, self.d))
+        inputs = inputs[..., tf.newaxis, :]
+        diff = inputs - cb
+        norm = tf.norm(diff, axis=-1)
+        index = tf.argmin(norm, axis=-1)
+        return index
+
+    @staticmethod
+    def sample(q_z):
+        one_hot = tf.one_hot(q_z)
+
+
+        return one_hot
+
+
+class Encoder(Model):
+    def __init__(self, d, hidden_layers, **kwargs):
+        super().__init__(**kwargs)
+        self.hidden_layers = []
+        for n in hidden_layers:
+            self.hidden_layers.append(Conv2D(n, 3, strides=(2, 2), padding='SAME', activation='relu'))
+        self.output_layer = Conv2D(d, 3, strides=(1, 1), padding='SAME')
+
+    def call(self, x, **kwargs):
+        for hidden_layer in self.hidden_layers:
+            x = hidden_layer(x)
+        z_e = self.output_layer(x)
+        return z_e
+
+
+class Decoder(Model):
+    def __init__(self, x_size, hidden_layers, **kwargs):
+        super().__init__(**kwargs)
+        self.hidden_layers = []
+        for n in hidden_layers:
+            self.hidden_layers.append(Conv2DTranspose(n, 3, strides=(2, 2), padding='SAME', activation='relu'))
+        self.output_layer = Conv2DTranspose(x_size, 3, strides=(1, 1), padding='SAME', activation='sigmoid')
+
+    def call(self, x, **kwargs):
+        for hidden_layer in self.hidden_layers:
+            x = hidden_layer(x)
+        x = self.output_layer(x)
+        return x
+
+
+class VQVAE(Model):
+    def __init__(self, d, k):
+        super(VQVAE, self).__init__()
+        self.encoder = Encoder(d, hidden_layers=[16, 32])
+        self.vq = VectorQuantization(k)
+        self.decoder = Decoder(1, hidden_layers=[32, 16])
+
+    def call(self, inputs, **kwargs):
+        z_e = self.encoder(inputs)
+        q_z = self.vq(z_e)
+        z_q = self.vq.sample(q_z)
+        z_q = z_e + tf.stop_gradient(z_q - z_e)
+        x = self.decoder(z_q)
+        return x
+
+
+@tf.function
+def compute_loss(input, model):
+    ...
+
+
+if __name__ == '__main__':
+    ...
